940f1f4e6d6603b90310110da8f988b4310834f6
Bill Xiong <bill.xiong@activenetwork.com>
2021-03-15 14:26:13 +0800

    build

---
+++ packages/jest-runtime/build/__mocks__/createRuntime.js
@@ -0,0 +1,48 @@
+'use strict'; /**
+               * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+               *
+               * This source code is licensed under the BSD-style license found in the
+               * LICENSE file in the root directory of this source tree. An additional grant
+               * of patent rights can be found in the PATENTS file in the same directory.
+               */
+
+const path = require('path');
+
+module.exports = function createRuntime(filename, config) {
+  const NodeEnvironment = require('jest-environment-node');
+  const Runtime = require('../');var _require =
+
+  require('jest-config');const normalize = _require.normalize;
+
+  config = normalize(
+  Object.assign(
+  {
+    name: 'Runtime-' + filename.replace(/\W/, '-') + '-tests',
+    rootDir: path.resolve(path.dirname(filename), 'test_root') },
+
+  config),
+
+  {}).
+  options;
+
+  const environment = new NodeEnvironment(config);
+  environment.global.console = console;
+  return Runtime.createHasteMap(config, { maxWorkers: 1, resetCache: false }).
+  build().
+  then(hasteMap => {
+    const runtime = new Runtime(
+    config,
+    environment,
+    Runtime.createResolver(config, hasteMap.moduleMap));
+
+
+    runtime.__mockRootPath = path.join(config.rootDir, 'root.js');
+    runtime.__mockSubdirPath = path.join(
+    config.rootDir,
+    'subdir2',
+    'module_dir',
+    'moduleDirModule.js');
+
+    return runtime;
+  });
+};
